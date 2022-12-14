1de73838c8ff6be8e2d7130207b8106ac43a6343
Natnicha Tarat <118197278+natnichatarat@users.noreply.github.com>
2022-12-14 21:34:32 +0700

    Create 7.java

---
+++ Lab2/7.java
@@ -0,0 +1,11 @@
+class text {
+    public static void main(String[] args) {
+        String s1 = "opal";
+        String s2 = "Natnicha";
+        String s3 = s1+s2;
+        System.out.println(s3);
+        System.out.println(s3.length());
+        System.out.println(s3.toUpperCase());
+        System.out.print(s3.substring(4,7));
+    }
+}
