dfecac841fd5e26441b9e5fbb2a5dda568d5a719
Natnicha Tarat <118197278+natnichatarat@users.noreply.github.com>
2022-12-14 21:33:32 +0700

    Create 6.cs

---
+++ Lab2/6.cs
@@ -0,0 +1,14 @@
+using System;
+public class cs
+{
+    public static void Main(string[] args)
+    {
+        int age = 20;
+        String name = "Natnicha Tart";
+        String nickname = "opal";
+        
+        Console.WriteLine ("My name is {0}", name);
+        Console.WriteLine ("My nickname is {0}",nickname);
+        Console.WriteLine ("I'm {0} year old",age);
+    }
+}
