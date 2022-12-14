e76b92d92ffc0b3c87554b511f6717ac592f36ef
Natnicha Tarat <118197278+natnichatarat@users.noreply.github.com>
2022-12-14 21:35:25 +0700

    Create 8.php

---
+++ Lab2/8.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$name = 'alice';
+$education = 'Marketing';
+echo "$name is friends with sun <br> ";
+echo "She is an $education <br>";
+echo strlen($education);
+?> 
+
+</body>
+</html>
