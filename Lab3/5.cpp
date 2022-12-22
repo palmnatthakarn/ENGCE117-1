d98871df4de69cbe5d0611afefce36fc29179f08
Natnicha Tarat <118197278+natnichatarat@users.noreply.github.com>
2022-12-22 14:01:07 +0700

    Update 5.cpp

--- Lab3/5.cpp
+++ Lab3/5.cpp
@@ -1,40 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
-struct Car {
-    char model[5];
-	int price;
-	int sale;
+struct Book {
+    char title[20];
+	char author[20];
+	char subject[40];	
 };//end loop
-struct Car  *findsale(struct Car model[],int count);
+void text(struct Book *bo);
 int main(){
-	struct Car model[5];
-	strcpy(model[0].model,"Tesla");
-	model[0].price = 129000;
-	model[0].sale = 2400;
-	for ( int i = 1; i< 5; i++){
-		printf("Car %d : model,price,sale:",i);
-		scanf("%s %d %d", model[i].model,
-						 &model[i].price,	
-						 &model[i].sale);
-	}//end loop for
-     struct Car *mostsale ;
-     mostsale = findsale (model,5);
-     printf("The is sale Most %s %d %d",(*mostsale).model,
-     									(*mostsale).price,
-										(*mostsale).sale);
-	return 0;
-}//end loop
-struct Car  *findsale(struct Car model[],int count ){
-
-	int maxsale,maxI;
-	maxsale = model[0].sale;
-	maxI = 0;
-	for ( int i=1 ; i < count ; i++){
-		if (model[i].sale > maxsale);{
-			maxsale = model[i].sale;	
-			 maxI = i;
-		}
-	}
-	return &model[maxI];
-};
+	struct  Book b;
+	text(&b);
+	printf("%s\n ",b.title);
+	printf("by %s\n",b.author);
+	printf("subject is %s",b.subject);
+}
+void text(struct Book *bo) {
+	printf("I read");
+	gets(bo->title);
+	printf("who is composed");
+	gets(bo->author);
+	printf("what is subject");
+	gets(bo->subject);
+}
